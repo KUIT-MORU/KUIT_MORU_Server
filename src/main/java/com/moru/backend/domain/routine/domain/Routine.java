a56b9e6e35f6964d9e39659099f42e56dfa8fb60
Minju <angertiger0941@gmail.com>
2025-06-30 02:49:31 +0900

    feat: add Routine Entity Model

---
+++ src/main/java/com/moru/backend/domain/routine/domain/Routine.java
@@ -0,0 +1,60 @@
+package com.moru.backend.domain.routine.domain;
+
+import com.moru.backend.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "routine")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Routine {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 10, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean isSimple;
+
+    @Column(nullable = false)
+    private boolean isPublic;
+
+    @Column(nullable = false)
+    private Integer likeCount;
+
+    @Column(nullable = false)
+    private LocalTime requiredTime;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean status = true;
+}
