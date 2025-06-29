9acb2d77ecb2bb133adb21ec3d1ffa8c163fc5a8
Minju <angertiger0941@gmail.com>
2025-06-30 02:49:39 +0900

    feat: add RoutineStep Entity Model

---
+++ src/main/java/com/moru/backend/domain/routine/domain/RoutineStep.java
@@ -0,0 +1,46 @@
+package com.moru.backend.domain.routine.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "routine_step")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoutineStep {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rotine_id", nullable = false)
+    private Routine routine;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer order;
+
+    @Column
+    private LocalTime estimatedTime;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
