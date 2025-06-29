c3f03ba694b224422b10a790dc286ab89697fd60
Minju <angertiger0941@gmail.com>
2025-06-30 02:52:01 +0900

    feat: add RoutineLog Entity Model

---
+++ src/main/java/com/moru/backend/domain/routine/domain/RoutineLog.java
@@ -0,0 +1,57 @@
+package com.moru.backend.domain.routine.domain;
+
+import com.moru.backend.domain.user.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "routine_log")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoutineLog {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "routine_id", nullable = false)
+    private Routine routine;
+
+    @Column(nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(nullable = false)
+    private LocalDateTime endedAt;
+
+    @Column(nullable = false)
+    private LocalTime totalTime;
+
+    @Column(nullable = false)
+    private boolean isSimple;
+
+    @Column(nullable = false)
+    private boolean isCompleted;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
