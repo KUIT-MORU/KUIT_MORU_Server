33992649882e0ef5736dba54b840f9c28833cd75
Minju <angertiger0941@gmail.com>
2025-06-30 02:50:16 +0900

    feat: add RoutineSchedule Entity Model, DayOfWeek Enum

---
+++ src/main/java/com/moru/backend/domain/routine/domain/RoutineSchedule.java
@@ -0,0 +1,33 @@
+package com.moru.backend.domain.routine.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "routine_schedule")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoutineSchedule {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "routine_id", nullable = false)
+    private Routine routine;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private LocalTime time;
+}
