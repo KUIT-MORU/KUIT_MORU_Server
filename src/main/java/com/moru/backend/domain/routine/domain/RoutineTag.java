1328ba023a076117d7cb05f798ffc7204b108f0d
Minju <angertiger0941@gmail.com>
2025-06-30 02:49:51 +0900

    feat: add RoutineTag Entity Model

---
+++ src/main/java/com/moru/backend/domain/routine/domain/RoutineTag.java
@@ -0,0 +1,44 @@
+package com.moru.backend.domain.routine.domain;
+
+import com.moru.backend.domain.Tag.domain.Tag;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(
+        name = "routine_tag",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"routine_id", "tag_id"})
+)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RoutineTag {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "routine_id", nullable = false)
+    private Routine routine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id", nullable = false)
+    private Tag tag;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
