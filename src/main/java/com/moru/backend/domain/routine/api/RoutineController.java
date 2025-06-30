8348fbe430216b51b4137df123af571c6a4b1faf
KelseyKwon <nykwon7777@gmail.com>
2025-06-30 23:17:25 +0900

    feat : routine 생성 컨트롤러 추가

---
+++ src/main/java/com/moru/backend/domain/routine/api/RoutineController.java
@@ -0,0 +1,25 @@
+package com.moru.backend.domain.routine.api;
+
+import com.moru.backend.domain.routine.application.RoutineService;
+import com.moru.backend.domain.routine.dto.request.RoutineCreateRequest;
+import com.moru.backend.domain.routine.dto.response.RoutineResponse;
+import com.moru.backend.domain.user.domain.User;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/routines")
+@RequiredArgsConstructor
+public class RoutineController {
+    private final RoutineService routineService;
+
+    @PostMapping
+    public RoutineResponse createRoutine(
+            @Valid @RequestBody RoutineCreateRequest request,
+            @AuthenticationPrincipal User user
+    ) {
+        return routineService.createRoutine(request, user);
+    }
+} 
