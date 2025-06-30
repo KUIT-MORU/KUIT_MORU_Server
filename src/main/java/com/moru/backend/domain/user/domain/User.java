d38f986fcbbc5034813a05f5ee03b6a7cd5d8a13
Minju <angertiger0941@gmail.com>
2025-07-01 08:44:58 +0900

    feat: Auditing 활성화
    @EntityListeners(AuditingEntityListener.class): Spring Data JPA의 감사(Auditing) 기능을 활성화하기 위해 사용하는 어노테이션, @CreatedDate, @LastModifiedDate 같은 어노테이션이 자동으로 값이 채워지도록 해준다.

--- src/main/java/com/moru/backend/domain/user/domain/User.java
+++ src/main/java/com/moru/backend/domain/user/domain/User.java
@@ -7,12 +7,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "user")
 @Getter
 @NoArgsConstructor
