739ed745d7880461f6a40cd2c94e66539c88bdc4
KelseyKwon <nykwon7777@gmail.com>
2025-07-01 11:12:23 +0900

    fix : RoutineTag에 @EntityListeners추가

--- src/main/java/com/moru/backend/domain/routine/domain/RoutineTag.java
+++ src/main/java/com/moru/backend/domain/routine/domain/RoutineTag.java
@@ -8,11 +8,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(
         name = "routine_tag",
         uniqueConstraints = @UniqueConstraint(columnNames = {"routine_id", "tag_id"})
