cde45bde073bef2ce98c5d8d9bbb1634e3503118
Minju <angertiger0941@gmail.com>
2025-07-05 00:33:40 +0900

    fix: 정규표현식 수정

--- src/main/java/com/moru/backend/domain/auth/util/PasswordValidator.java
+++ src/main/java/com/moru/backend/domain/auth/util/PasswordValidator.java
@@ -2,7 +2,7 @@ package com.moru.backend.domain.auth.util;
 
 public class PasswordValidator {
     private static final String PASSWORD_PATTERN =
-            "^(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].{8,}$)";
+            "^(?=.*\\d)(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?]).{8,}$";
     public static boolean validatePassword(String password) {
         return password != null && password.matches(PASSWORD_PATTERN);
     }
