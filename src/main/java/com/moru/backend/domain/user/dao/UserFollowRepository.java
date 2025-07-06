2cea3b8e6d788f73739b77ac1112c94a96f57c2d
Minju <angertiger0941@gmail.com>
2025-07-07 00:15:32 +0900

    fix: UserFollowRepository 기본 키 타입 UUID로 수정

--- src/main/java/com/moru/backend/domain/user/dao/UserFollowRepository.java
+++ src/main/java/com/moru/backend/domain/user/dao/UserFollowRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface UserFollowRepository extends JpaRepository<UserFollow, Long> {
+public interface UserFollowRepository extends JpaRepository<UserFollow, UUID> {
     long countByFollowerId(UUID userId); //팔로잉 수
     long countByFollowingId(UUID userID); //팔로워 수
 }
