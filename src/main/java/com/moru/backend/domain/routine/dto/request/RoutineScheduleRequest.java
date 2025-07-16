e3535f637e24deae2dde80cff68993942b19ab8a
KelseyKwon <nykwon7777@gmail.com>
2025-07-16 10:17:21 +0900

    refactor : RoutineSchdeuleRequest에 @Schema 추가

--- src/main/java/com/moru/backend/domain/routine/dto/request/RoutineScheduleRequest.java
+++ src/main/java/com/moru/backend/domain/routine/dto/request/RoutineScheduleRequest.java
@@ -3,9 +3,11 @@ package com.moru.backend.domain.routine.dto.request;
 import com.moru.backend.domain.routine.domain.schedule.DayOfWeek;
 import java.time.LocalTime;
 import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record RoutineScheduleRequest(
         DayOfWeek dayOfWeek,
+        @Schema(description = "루틴 시간(HH:mm:ss)", example = "14:30:00")
         LocalTime time,
         Boolean alarmEnabled,
         String repeatType,
