fca8bc10a0dfcabe5770ad90391d18d9456734cf
KelseyKwon <nykwon7777@gmail.com>
2025-07-19 20:34:51 +0900

    fix : rmved LiveUserResponse

--- src/main/java/com/moru/backend/domain/routine/dto/response/LiveUserResponse.java
+++
@@ -1,8 +0,0 @@
-package com.moru.backend.domain.routine.dto.response;
-
-public record LiveUserResponse(
-    String username,
-    String profileImageUrl,
-    String motivationTag,
-    String feelUrl
-) {} 
