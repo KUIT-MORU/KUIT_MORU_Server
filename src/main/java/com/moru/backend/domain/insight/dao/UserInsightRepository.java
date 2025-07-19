4e21f63134bd2ae2ce1794515a9dc9fd5d4693ed
Minju <angertiger0941@gmail.com>
2025-07-20 01:22:52 +0900

    refactor: 필드 타입에 맞게 수정

--- src/main/java/com/moru/backend/domain/insight/dao/UserInsightRepository.java
+++ src/main/java/com/moru/backend/domain/insight/dao/UserInsightRepository.java
@@ -3,10 +3,10 @@ package com.moru.backend.domain.insight.dao;
 import com.moru.backend.domain.insight.domain.UserInsight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 public interface UserInsightRepository extends JpaRepository<UserInsight, UUID> {
-    List<UserInsight> findByUpdatedAtAfter(LocalDate localDate);
+    List<UserInsight> findByUpdatedAtAfter(LocalDateTime localDateTime);
 }
