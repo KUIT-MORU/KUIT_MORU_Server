4b89ba2c1ebdb1214976ca8088c5dcf97fea33ad
Minju <angertiger0941@gmail.com>
2025-07-26 03:12:38 +0900

    feat: 회원가입시 이메일, 패스워드만 받도록 수정 - User 엔티티의 닉네임, 성별, 생년월일 필드 nullable로 변경 - 회원가입 요청의 필드를 이메일, 비밀번호로

--- src/main/java/com/moru/backend/domain/auth/application/SignupService.java
+++ src/main/java/com/moru/backend/domain/auth/application/SignupService.java
@@ -21,41 +21,19 @@ import java.util.UUID;
 public class SignupService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final UserFavoriteTagService userFavoriteTagService;
-    private final S3Service s3Service;
 
     @Transactional
     public void signup(SignupRequest request) {
         if(userRepository.existsByEmail(request.email())) {
             throw new CustomException(ErrorCode.USER_EMAIL_ALREADY_EXISTS);
         }
-        if(userRepository.existsByNickname(request.nickname())) {
-            throw new CustomException(ErrorCode.USER_NICKNAME_ALREADY_EXISTS);
-        }
-
-        // === 이미지 이동 처리 ===
-        String imageKey = null;
-        if(request.profileImageUrl() != null && !request.profileImageUrl().isBlank()) {
-            imageKey = s3Service.moveToRealLocation(request.profileImageUrl(), S3Directory.PROFILE);
-        }
 
         User user = User.builder()
-                .id(UUID.randomUUID())
                 .email(request.email())
                 .password(passwordEncoder.encode(request.password()))
-                .nickname(request.nickname())
-                .gender(request.gender())
-                .birthday(request.birthday())
-                .bio(request.bio())
-                .profileImageUrl(imageKey)
                 .status(true)
                 .build();
-        userRepository.save(user);
 
-        // 관심 태그 저장
-        if(request.tagIds() != null && !request.tagIds().isEmpty()) {
-            FavoriteTagRequest tagRequest = new FavoriteTagRequest(request.tagIds());
-            userFavoriteTagService.addFavoriteTag(user, tagRequest);
-        }
+        userRepository.save(user);
     }
 }
