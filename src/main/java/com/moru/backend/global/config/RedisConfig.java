9420e45ae360d2bb8e35aeb2bfbd2efedd5606c7
KelseyKwon <nykwon7777@gmail.com>
2025-08-04 22:37:49 +0900

    fix : RedisConfig 생성

---
+++ src/main/java/com/moru/backend/global/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.moru.backend.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String,String> redisTemplate(RedisConnectionFactory f) {
+        RedisTemplate<String,String> t = new RedisTemplate<>();
+        t.setConnectionFactory(f);
+        t.setKeySerializer(new StringRedisSerializer());
+        t.setValueSerializer(new StringRedisSerializer());
+        return t;
+    }
+}
